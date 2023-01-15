bf4abefb02912f428de19043e7c885934e66d709
Norbs <97234527+norbertcsipak@users.noreply.github.com>
2023-01-15 08:39:33 +0000

    counter function made with react native

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import CounterScreen from './components/screens/CounterScreen';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <CounterScreen />
     </View>
   );
 }
